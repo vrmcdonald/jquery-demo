67b7a09de5e7ae9c4be353d9415c563670034b47
Vincent McDonald <erosemcdonald@ufl.edu>
2018-11-09 23:19:52 -0500

    Completed assignment.

---
+++ mcdonald_web_folder/scripts/fade_script.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+  //Orange flowers
+  $('#bo').on('click', () => {
+   $('#orange').fadeToggle('slow');
+ });
+ //Sunflowers
+ $('#so').on('click', () => {
+  $('#sunflower').fadeToggle('slow');
+ });
+ //Tulip
+ $('#to').on('click', () => {
+  $('#tulip').fadeToggle('slow');
+});
+//Flowers and herb
+$('#go').on('click', () => {
+ $('#gwp').fadeToggle('slow');
+});
+
+});
